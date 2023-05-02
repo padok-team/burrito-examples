5452e700f2d1451eb3c952648ca2496ea39a37cc
spoukke <sacha.bernheim@hey.com>
2023-05-02 17:30:10 +0200

    feat: make code fail

--- modules/random-pets/main.tf
+++ modules/random-pets/main.tf
@@ -2,3 +2,11 @@ resource "random_pet" "this" {
   for_each = toset(var.pets)
   length = 1
 }
+
+resource "random_pet" "default" {
+  length = 1
+}
+
+resource "random_pet" "default" {
+  length = 1
+}
